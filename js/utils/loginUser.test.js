29588f5673d0a4475633af325e65d5c6fd5371c8
hreinn <hreinngy@gmail.com>
2022-11-07 08:26:43 +0100

    Adds jest loginUser test

--- js/utils/loginUser.test.js
+++ js/utils/loginUser.test.js
@@ -1,1 +1,28 @@
 import { logInUser } from './loginUser';
+
+const MOCK_API_URL = '';
+
+const MOCK_USER_DATA = {
+    email: 'someone@noroff.no',
+    password: 'somepassword343',
+};
+
+const MOCK_RESPONSE = {
+    name: 'someone',
+    email: 'someone@noroff.no',
+    banner: null,
+    avatar: null,
+    accessToken: 'sometokentouse3434',
+};
+
+global.fetch = jest.fn(() =>
+    Promise.resolve({
+        json: () => Promise.resolve(MOCK_RESPONSE),
+    })
+);
+
+it('should return token when provided with valid credentials ', async () => {
+    const response = await logInUser(MOCK_API_URL, MOCK_USER_DATA);
+    const accessToken = response.accessToken;
+    expect(accessToken).toBeDefined();
+});
